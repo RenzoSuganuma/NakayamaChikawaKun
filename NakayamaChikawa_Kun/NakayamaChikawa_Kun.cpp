982e89b596bf1ef15481f5af33ce76f43c76f053
RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2023-09-03 22:21:48 +0900

    アップデート

--- NakayamaChikawa_Kun/NakayamaChikawa_Kun.cpp
+++ NakayamaChikawa_Kun/NakayamaChikawa_Kun.cpp
@@ -1,12 +1,43 @@
 ﻿#include <stdio.h>
 #include <Windows.h>
+#include<WinUser.h>
 #pragma comment(lib, "winmm.lib")   // MSVC 用
-void introductionsequence() {
-    printf("はーぴー：リラックスできる音楽を流しますね。\n");
-    printf("はーぴー：～～～♪\n");
-    PlaySound(TEXT("assets\\gb1.wav"), NULL, SND_FILENAME);
-    printf("はーぴー：精一杯演奏してみました。\n");
+int DisplayResourceNAMessageBox()
+{
+    int msgboxID = MessageBox(
+        NULL,
+        (LPCWSTR)L"ちいかわとなかやまきんにくんは好きかい？（＾ω＾）",
+        (LPCWSTR)L"ちいいいいいいかわああ",
+        MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON1
+    );
+    switch (msgboxID)
+    {
+    case IDNO:
+        PlaySound(TEXT("assets\\chikawa_yah.wav"), NULL, SND_FILENAME);
+        PlaySound(TEXT("assets\\muscle_dah.wav"), NULL, SND_FILENAME);
+        break;
+    case IDYES:
+        PlaySound(TEXT("assets\\chikawa_wa.wav"), NULL, SND_FILENAME);
+        PlaySound(TEXT("assets\\muscle_power.wav"), NULL, SND_FILENAME);
+        break;
+    }
+    return msgboxID;
 }
 int main() {
-    introductionsequence();
+    for (int i = 0; i < 5; i++) {
+        int msgboxID = MessageBox(
+            NULL,
+            (LPCWSTR)L"コンピュータでエラーが見つかりました",
+            (LPCWSTR)L"致命的なエラー",
+            MB_ICONWARNING
+        );
+        switch (msgboxID)
+        {
+        case IDNO:
+            break;
+        case IDYES:
+            break;
+        }
+    }
+    int i = DisplayResourceNAMessageBox();
 }
